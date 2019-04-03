d3f58a88eecc31981d2a1e5cbe6b8e54d7ff084a
ALXSMORA\Alexis Mora <alexis.mora1@icloud.com>
2019-04-03 11:34:24 -0600

    Index Root del Framework * Se agregan comentarios más especificos * Implementación del estandard de codigo de  Symfony

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <?php 
 /**
- *  Index root
+ * Este archivo forma parte del Framework Nimter.
+ *
+ * Para más información acerca de los derechos de autor y la licencia, ver el archivo LICENSE.
  *  
  * PHP versión 7.1.3
  *
- * @package Nimter Framework
- * @author Alexis Mora
+ * @package Nimter\Core\controllers
+ * @author Alexis Mora <alexis.mora1v@gmail.com>
  * @version 1.2.0
- * 
- **/
+ */
 
 //Define la ruta por defecto del framework
 define('__PATH__', '');
 
 //Carga el framework
-require_once (__PATH__.'Nimter/Core/init/init.php');
-
-?>
+require_once __PATH__ . 'Nimter/Core/init/Init.php';
