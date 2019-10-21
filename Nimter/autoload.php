5c90e86a9684f90139308422821a3b226d944cb0
ALXSMORA\Alexis Mora <alexis.mora1@icloud.com>
2019-10-21 10:46:49 -0500

    Autoload - Se eliminan los espacios en blanco - Se actualizan los comentarios - Se usa el estandar elegido para el codigo

--- Nimter/autoload.php
+++ Nimter/autoload.php
@@ -1,22 +1,19 @@
 <?php
 /**
  * Clase para caragar las clases/modulos del Core del Framework
- *  
- * PHP versión 7.0
+ *
+ * PHP versión 7.1.3
  *
  * @package Nimter
- * @version 1.2.0
+ * @version 1.3.0
  * @author Alexis Mora
- *  
+ *
  **/
 
-spl_autoload_register(function ($modules) 
-{
+spl_autoload_register(function ($modules) {
     $modules = str_replace('\\', '/', $modules);
 
-    if ( is_readable($modules . '.php') ) 
-    {
+    if (is_readable($modules . '.php')) {
         require_once $modules . '.php';
     }
 });
-?>
