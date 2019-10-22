9ae4b0b9cc876116da3aae6f3ab1a1dda4d1c493
ALXSMORA\Alexis Mora <alexis.mora1@icloud.com>
2019-10-22 13:39:34 -0500

    Actualización de la clase Conn2db
    Correción de los namespaces para hacer un llamado más organico a las
    funciones. Se agrega el charset de codificación en el conector y el
    horario local a través del primer comando ejecutado en al abrir la
    conexión.

--- Nimter/Core/Helpers/Conn2db.php
+++ Nimter/Core/Helpers/Conn2db.php
@@ -6,15 +6,15 @@
  *
  * PHP versión 7.1.3
  *
- * @package Nimter\Core\router
+ * @package Nimter\Core\Helpers
  * @author Alexis Mora <alexis.mora1v@gmail.com>
- * @version 1.2.0
+ * @version 1.3.0
  */
 
 namespace Nimter\Core\Helpers;
 
 use \PDO;
-use Nimter\Core\Init\ConfigReader as config;
+use Nimter\Core\Init\ConfigReader;
 
 /**
  * Class Conn2db
@@ -37,6 +37,8 @@ class Conn2db
     protected $DBpwd;
     # @var, Codificación de la base de datos
     protected $DBCodification;
+    # @var, Hora local de la base de datos
+    private $DBLocale;
     # @object, Objeto PDO
     protected $pdo;
     # @array, Parametros para la consulta
@@ -52,15 +54,16 @@ class Conn2db
     public function __construct()
     {
         //Carga la configuracion del framework
-		$config = config::config();
+		$config = ConfigReader::config();
 
-        $this->driver   = $config['database']['driver'];
-        $this->host     = $config['database']['host'];
+        $this->DBdriver = $config['database']['driver'];
+        $this->DBhost   = $config['database']['host'];
         $this->DBport   = $config['database']['port'];
         $this->DBname   = $config['database']['name'];
         $this->DBuser   = $config['database']['user'];
         $this->DBpwd    = $config['database']['pwd'];
-        $this->DBCodification    = $config['database']['codification'];
+        $this->DBCodification = $config['database']['codification'];
+        $this->DBLocale = $config['database']['locale'];
         $this->params   = array();
         $this->connection();
     }
@@ -73,13 +76,13 @@ class Conn2db
     protected function connection()
     {
         try {
-            $connector = $this->driver . ':host=' . $this->host . ';dbname=' . $this->DBname;
+            $connector = $this->DBdriver . ':host=' . $this->DBhost . ';dbname=' . $this->DBname . ';charset=' . $this->DBCodification;
 
             $attributes = array(
                 PDO::ATTR_PERSISTENT => false,
                 PDO::ATTR_EMULATE_PREPARES => false,
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES '" . $this->DBCodification . "'"
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET LC_TIME_NAMES='" . $this->DBLocale . "'"
             );
 
             $this->pdo = new PDO($connector, $this->DBuser, $this->DBpwd, $attributes);
